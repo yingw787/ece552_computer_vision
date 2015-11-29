00007c567fd28a80d58dd8528b4b7f3c64f7f8f4
Ying Wang <yingw787@gmail.com>
2015-11-29 00:59:36 -0500

    really just getting pip to vm

---
+++ algorithm_benchmarks/c++/hello.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+#include "/usr/local/Cellar/opencv/2.4.12/include/opencv2/highgui/highgui.hpp"
+#include "/usr/local/Cellar/opencv/2.4.12/include/opencv2/core/core.hpp"
+
+
+int main()
+{
+  std::cout << "Hello World!";
+}
