8e749370692696d35c5a1145f27ce5020ceea5c6
Ying Wang <yingw787@gmail.com>
2015-11-30 15:14:54 -0500

    nvm about using the vm no graphics ability -____-

--- algorithm_benchmarks/c++/sobel.cpp
+++ algorithm_benchmarks/c++/sobel.cpp
@@ -1,7 +1,9 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include "/usr/local/Cellar/opencv/2.4.12/include/opencv2/imgproc/imgproc.hpp"
+#include "/usr/local/Cellar/opencv/2.4.12/include/opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
+
+
 
 using namespace cv;
 
@@ -11,7 +13,7 @@ int main( int argc, char** argv )
 
   Mat src, src_gray;
   Mat grad;
-  char* window_name = "Sobel Demo - Simple Edge Detector";
+  // char* window_name = "Sobel Demo - Simple Edge Detector";
   int scale = 1;
   int delta = 0;
   int ddepth = CV_16S;
@@ -30,7 +32,7 @@ int main( int argc, char** argv )
   cvtColor( src, src_gray, CV_BGR2GRAY );
 
   /// Create window
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+  namedWindow( "window_name", CV_WINDOW_AUTOSIZE );
 
   /// Generate grad_x and grad_y
   Mat grad_x, grad_y;
@@ -49,7 +51,7 @@ int main( int argc, char** argv )
   /// Total Gradient (approximate)
   addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
 
-  imshow( window_name, grad );
+  imshow( "window_name", grad );
 
   waitKey(0);
 
