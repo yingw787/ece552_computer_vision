ddc81a977fbe1ced2e92cdd0ce3b8aa29e2aa233
Ying Wang <yingw787@gmail.com>
2015-12-03 21:52:26 -0500

    changed appendices

--- algorithm_benchmarks/c++/helloworld.cpp
+++ algorithm_benchmarks/c++/helloworld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
-#include "./src/papi.h"
+#include "papi.h"
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+
+	int temp = PAPI_L1_DCM;
+
+	int numCounters = PAPI_VER_CURRENT;
+
+
+	std::cout << temp;
+	std::cout << "\n";
+
+   	std::cout << numCounters;
+	std::cout << "\n";
+	std::cout << "Hello, world!\n";
 }
