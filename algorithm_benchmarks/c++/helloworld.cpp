6a90c99c915ec471399e61ded27dc06b6d28aa60
Ying Wang <yingw787@gmail.com>
2015-12-04 00:29:56 -0500

    latest commit

--- algorithm_benchmarks/c++/helloworld.cpp
+++ algorithm_benchmarks/c++/helloworld.cpp
@@ -2,18 +2,26 @@
 #include "papi.h"
 #include <stdio.h>
 
+int PAPI_start_counters(int *events, int array_len); /**< start counting hardware events */
+int PAPI_stop_counters(long long * values, int array_len); /**< stop counters and return current counts */
+
+
 int main()
 {
+	int Events[1] = { PAPI_TOT_CYC };
+	long long values[1];
 
-	int temp = PAPI_OK; 
+	if (PAPI_start_counters(Events, 2) != PAPI_OK)
+	{
+//		std::cout "not okay to start";
+	}
 
-	std::cout << temp;
-	std::cout << "\n";
-
-	std::cout << "\n";
 	std::cout << "Hello, world!\n";
 
-
+	if (PAPI_stop_counters(values, 2) != PAPI_OK)
+	{
+//		std::cout "not okay to stop";
+	}
 
 
 }
